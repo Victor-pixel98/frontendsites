6e2e335e9a15ff9c9b572807b60ce5eaeb7e0082
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2020-12-18 13:40:26 +0600

    Cappadokia site
    Здесь я залил верстку сайта Кападокии

---
+++ Kapadokia/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.fairy-tail__slider, .out-trip__slider').slick({
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
+        autoplay: true,
+        fade: true
+
+    });
+});
+
+
